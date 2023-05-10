6c6c5a103d357f1011e3ba5abe803c35e2388621
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-05-10 12:06:44 -0700

    rename parts page and add route handling

--- routes/partsListPage.js
+++ routes/partsListPage.js
@@ -1,5 +1,14 @@
-var {
-    database
-  } = include('databaseConnection');
-  
-  const partsCollections = database.db(mongodb_database).collection('BOB');
+const express = require('express');
+router = express.Router;
+
+
+module.exports = function (app) {
+  app.get('/partsListPage', (req, res) => {
+
+    // TODO: After making the schemas and models,
+    // create function that iterates through the models of parts and returns a card for each part
+
+
+    res.render('partsListPage');
+  })
+}
