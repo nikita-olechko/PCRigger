ed71beb3f7019d9b837b8d423f227accc7ad774e
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-10 14:27:36 -0700

    hash security questions

--- routes/signUp.js
+++ routes/signUp.js
@@ -12,20 +12,31 @@ module.exports = function (app, userCollection, saltRounds, Joi, bcrypt) {
             password,
             security_question_1,
             security_question_2,
-            security_question_3
+            security_question_3,
+            security_answer_1,
+            security_answer_2,
+            security_answer_3
         } = req.body;
 
         // Validate input
         const schema = Joi.object({
             username: Joi.string().alphanum().min(3).max(20).required(),
-            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+            security_answer_1: Joi.string().required(),
+            security_answer_2: Joi.string().required(),
+            security_answer_3: Joi.string().required()
+
         });
         const validationResult = schema.validate({
             username,
-            password
+            password,
+            security_answer_1,
+            security_answer_2,
+            security_answer_3
         });
+
         if (validationResult.error) {
-            res.status(400).send(`Invalid username or password. <a href="/">Go back to home</a>`);
+            res.status(400).send(`Invalid username or password or security questions. <a href="/">Go back to home</a>`);
             return;
         }
 
@@ -41,6 +52,14 @@ module.exports = function (app, userCollection, saltRounds, Joi, bcrypt) {
         // Hash password
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+        const hashedAnswer1 = await bcrypt.hash(security_answer_1, saltRounds);
+
+        const hashedAnswer2 = await bcrypt.hash(security_answer_2, saltRounds);
+
+        const hashedAnswer3 = await bcrypt.hash(security_answer_3, saltRounds);
+
+
+
         var newDate = new Date();
         // Create new user
         const newUser = {
@@ -48,8 +67,11 @@ module.exports = function (app, userCollection, saltRounds, Joi, bcrypt) {
             password: hashedPassword,
             user_type: 'user',
             security_question_1: security_question_1,
+            security_answer_1: hashedAnswer1,
             security_question_2: security_question_2,
+            security_answer_2: hashedAnswer2,
             security_question_3: security_question_3,
+            security_answer_3: hashedAnswer3,
             createdAt: newDate.toLocaleDateString() + " @ " + newDate.toLocaleTimeString()
         };
         await userCollection.insertOne(newUser);
