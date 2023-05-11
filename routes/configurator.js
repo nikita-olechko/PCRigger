364d922cccbfa24443bc5a7246b77dded8c38e63
Cameron Postnikoff <CameronJP@protonmail.com>
2023-05-11 14:06:02 -0700

    Add BASIC part-population to configurator

--- routes/configurator.js
+++ routes/configurator.js
@@ -1,11 +1,45 @@
 var express = require('express');
 router = express.Router();
+const mongoose = require('mongoose');
+router = express.Router;
+const buildsModel = require('../models/buildsModel');
+const utils = require('../utils');
+const path = require('path');
+const mime = require('mime');
+
+const mongodb_database = process.env.MONGODB_DATABASE;
+
+var {
+    database
+} = include('databaseConnection');
+
+const prebuilts = database.db(mongodb_database).collection('pcbuilds')
 
 module.exports = function(app){
-    app.get('/configurator', (req, res) =>{
-        desiredCategory: req.query.desiredCategory;
-        res.render('configurator', {
-            // desiredCategory: desiredCategory
+    app.use(express.static(path.join(__dirname, 'scripts'), {
+        setHeaders: (res, filePath) => {
+            const mimeType = mime.getType(filePath);
+            if (mimeType) {
+            res.set('Content-Type', mimeType);
+            }
+        }
+        }));
+        
+    app.post('/configurator', async (req, res) => {
+        
+        var desiredCategory = req.body.formId;
+
+
+        await prebuilts.find({class: `${desiredCategory}`}).toArray(function (err, result) {
+            if (err) {
+                throw err
+            } else {
+                // console.log(result)
+                res.render('configurator', {
+                    builds: result,
+                    index: 0
+                })
+            };
         })
-    })
+    });
 }
