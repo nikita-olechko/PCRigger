612af4c45c6d53b86dd6eb220a57f96124dce538
Cameron Postnikoff <CameronJP@protonmail.com>
2023-05-12 00:24:13 -0700

    debug categories route

--- routes/members.js
+++ routes/members.js
@@ -5,7 +5,7 @@ module.exports = function(app){
     app.get('/members', (req, res) =>{
         const buttonsData = [
             { route: 'prebuiltOptions', description: 'Build Your PC' },
-            { route: '#', description: 'Compare Parts' },
+            { route: 'categories', description: 'Compare Parts' },
             { route: '#', description: 'Info' }
         ];
         res.render('members', { buttons: buttonsData });
