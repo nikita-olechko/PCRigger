3ac29808b262c2051d6082c432c8932be0f5aba0
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-11 21:48:16 -0700

    write builds to user database

--- routes/configurator.js
+++ routes/configurator.js
@@ -38,13 +38,29 @@ module.exports = function (app, userCollection) {
         console.log(build)
         const userID = req.session.user.username;
         //get user profile
+        await userCollection.updateOne(
+            { username: userID },
+            { $push: { favourites: build } },
+            function (err, res) {
+                if (err) throw err;
+                console.log('build added to user!');
+            }
+        );
         const userProfile = await userCollection.findOne({
             username: userID
         });
-        // console.log(userProfile)
+
+        // userProfile.favourites = [...userProfile.favourites, build]
+
+        // await userCollection.deleteOne({ username: userID }, () => {
+        //     console.log("User deleted");
+        // });
+
+        // await userCollection.insertOne(userProfile);
         //add build to userProfile
-        userProfile.favourites.push(build);
-        // console.log(userProfile)
-        // console.log(userProfile.favourites)
+        console.log("Here is the user profile with the new build added")
+        console.log(userProfile)
+        console.log("Here is the favourites section")
+        console.log(userProfile.favourites)
     });
 }
