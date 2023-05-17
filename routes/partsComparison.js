e0dc5930dc4b0d333d0a148f87a74db32b734bfe
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-05-16 23:25:56 -0700

    Finish Comparison page "table"

--- routes/partsComparison.js
+++ routes/partsComparison.js
@@ -7,8 +7,10 @@ module.exports = function(app){
     app.post('/compare', (req, res) =>{
         const firstComparedPart = req.body.selectedFirstPart;
         const secondComparedPart = req.body.selectedSecondPart;
+        const partsType = req.body.partsType;
+        console.log(partsType);
         console.log(firstComparedPart);
         console.log(secondComparedPart);
-        res.render('comparisonPage',{firstComparedPart: firstComparedPart, secondComparedPart: secondComparedPart});
+        res.render('comparisonPage',{firstComparedPart: firstComparedPart, secondComparedPart: secondComparedPart, partsType: partsType});
     })
 }
