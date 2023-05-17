a70d3b54926bdc6539684e4846a77aaee7f8fa4d
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-17 16:11:59 -0700

    Create functioning route that populates build fields

--- routes/specificBuildInfo.js
+++ routes/specificBuildInfo.js
@@ -6,19 +6,24 @@ module.exports = function (app, userCollection) {
 
     app.post('/specificBuildInfo', async (req, res) => {
         var build = JSON.parse(req.body.build)
-        console.log("CPU Is" + build.parts.cpu)
+        // console.log("CPU Is" + build.parts.cpu)
 
         try { // console.log("At configurator post route")
-            const cpus = await build.parts.cpu
-            const gpus = await build.parts.gpu
-            const memory = await build.parts.ram
-            const storage = await build.parts.storage
-            const motherboards = await build.parts.motherboard
-            const powerSupply = await build.parts.powersupply
-            const cases = await build.parts.case
-            const cpuCoolers = await build.parts.cpuCooler
+            const cpus = Array.isArray(await build.parts.cpu) ? await build.parts.cpu : [await build.parts.cpu];
+            const gpus = Array.isArray(await build.parts.gpu) ? await build.parts.gpu : [await build.parts.gpu];
+            const memory = Array.isArray(await build.parts.ram) ? await build.parts.ram : [await build.parts.ram];
+            const storage = Array.isArray(await build.parts.storage) ? await build.parts.storage : [await build.parts.storage];
+            const motherboards = Array.isArray(await build.parts.motherboard) ? await build.parts.motherboard : [await build.parts.motherboard];
+            const powerSupply = Array.isArray(await build.parts.powersupply) ? await build.parts.powersupply : [await build.parts.powersupply];
+            const cases = Array.isArray(await build.parts.case) ? await build.parts.case : [await build.parts.case];
+            const cpuCoolers = Array.isArray(await build.parts.cpuCooler) ? await build.parts.cpuCooler : [await build.parts.cpuCooler];
+
+
+            console.log("CPUS: " + cpus)
+            console.log("memory: " + memory)
 
             res.render('info', {
+                build: build,
                 cpus: cpus,
                 gpus: gpus,
                 memory: memory,
@@ -31,6 +36,7 @@ module.exports = function (app, userCollection) {
         } catch (error) {
             console.error('Error retrieving data from MongoDB:', error);
             res.status(500).send('Internal Server Error');
+            res.render("500");
         }
     });
 }
