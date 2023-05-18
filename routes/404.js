9c7b2af968f5b40ac0492bca98cebc1f5944d513
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-17 17:39:02 -0700

    Commit 404 route

---
+++ routes/404.js
@@ -0,0 +1,8 @@
+var express = require('express');
+router = express.Router();
+
+module.exports = function(app){
+    app.get("*", (req, res) => {
+        res.status(404).render('404');
+      })
+}
