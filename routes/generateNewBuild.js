27781d92778e8a488f35c2dd7c41620a06297a7d
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-18 19:25:43 -0700

    Get selected values from AI_build advanced filtering

--- routes/generateNewBuild.js
+++ routes/generateNewBuild.js
@@ -2,7 +2,13 @@ const express = require('express');
 router = express.Router();
 
 module.exports = function (app, userCollection) {
+
     app.post('/generateNewBuild', async (req, res) => {
-        
+        selectedParts = JSON.parse(req.body.selectedParts)
+        console.log(selectedParts)
+    });    
+    
+    app.post('/renderBuildFilters', async (req, res) => {
+        res.render('AI_build')
     });
 }
