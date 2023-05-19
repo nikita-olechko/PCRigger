b87ee0786611333b73feaa65c1e9d7e0093f590f
Nikita Olechko <nikita.olechko@gmail.com>
2023-05-18 20:07:45 -0700

    Prevent new build with no filters and add notification

--- routes/generateNewBuild.js
+++ routes/generateNewBuild.js
@@ -6,9 +6,25 @@ module.exports = function (app, userCollection) {
     app.post('/generateNewBuild', async (req, res) => {
         selectedParts = JSON.parse(req.body.selectedParts)
         console.log(selectedParts)
+
+        const createPromptFromParts = (parts) => {
+            for (let i = 0; i < parts.length; i++) {
+                if (parts[i].length == 0) {
+                    return "Please select a part for each category"
+                }
+            }
+        }
+    });
+
+
+
+
+
+    app.post('/renderBuildFilters', async (req, res) => {
+        res.render('AI_build', { noFactorsChosen: false })
     });    
     
-    app.post('/renderBuildFilters', async (req, res) => {
-        res.render('AI_build')
+    app.post('/advancedFilterWithNotification', async (req, res) => {
+        res.render('AI_build', { noFactorsChosen: true })
     });
 }
