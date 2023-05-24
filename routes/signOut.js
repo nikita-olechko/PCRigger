71b4d91e018f800822fbe5fa79899eebd9915e10
Cameron Postnikoff <CameronJP@protonmail.com>
2023-05-24 13:44:48 -0700

    Add missing semicolons

--- routes/signOut.js
+++ routes/signOut.js
@@ -11,7 +11,7 @@ module.exports = async function (app) {
             }
             res.clearCookie('connect.sid');
             res.redirect('/');
-            return
+            return;
         });
     });
 
@@ -25,7 +25,7 @@ module.exports = async function (app) {
             }
             res.clearCookie('connect.sid');
             res.redirect('/');
-            return
+            return;
         });
     });
-}
+};
