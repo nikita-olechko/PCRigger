1bb3c088cd9a78119931a5bde227137413241461
Brian Lee <clee689@my.bcit.ca>
2023-05-24 22:32:45 -0700

    refactor and comment members and OpenAIcall js

--- routes/OpenAIcall.js
+++ routes/OpenAIcall.js
@@ -1,26 +1,32 @@
-const axios = require('axios')
+const axios = require('axios');
 
 const url = 'https://api.openai.com/v1/chat/completions';
 const api_key = process.env.API_KEY;
-const headers = {
+const headers = 
+// setting the headers for the API call
+{
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${api_key}`
 };
 
 async function makeAPIRequest(prompt) {
-    const data = {
+    const data = 
+    // setting the data for the API call
+    {
         model: 'gpt-3.5-turbo',
         messages: [
             { role: 'user', content: prompt }
         ]
     };
     try {
+      // making the API call
       const response = await axios.post(url, data, { headers });
+      // getting the answer from the API response object
       const answer = response.data.choices[0].message.content;
-      return answer
+      return answer;
     } catch (error) {
-      return null
+      return null;
     }
 }
 
-module.exports = makeAPIRequest
+module.exports = makeAPIRequest;
