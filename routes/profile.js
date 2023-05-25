5aa1ae08ef670a1d9abae438e3a9a79ee3e1cf87
Brian Lee <clee689@my.bcit.ca>
2023-05-24 22:40:22 -0700

    refactor and comment profile js

--- routes/profile.js
+++ routes/profile.js
@@ -15,7 +15,7 @@ const userCollection = database.db(mongodb_database).collection('users');
 module.exports = async function (app, Joi, bcrypt, saltRounds) {
 
     function isValidSession(req, res) {
-        console.log("checking session")
+        console.log("checking session");
         if (req.session.user) {
             return true;
         }
@@ -29,7 +29,7 @@ module.exports = async function (app, Joi, bcrypt, saltRounds) {
         }
 
         user_profile = await userCollection.findOne({ username: req.session.user.username });
-        builds = user_profile.favourites
+        builds = user_profile.favourites;
         // console.log(user_profile.favourites)
 
 
@@ -47,34 +47,40 @@ module.exports = async function (app, Joi, bcrypt, saltRounds) {
             builds: builds
         });
 
-    })
+    });
 
     app.get('/edit', async (req, res) => {
         if (!isValidSession(req)) {
             res.redirect('/login');
             return;
         }
-        const buttonsData = [
+        const buttonsData = 
+        // profile edit buttons
+        [
             { route: 'username_edit', description: 'Username' },
             { route: 'password_edit', description: 'Password' },
-        ]
-        res.render('profile_edit', { buttons: buttonsData })
-    })
+        ];
+        res.render('profile_edit', { buttons: buttonsData });
+    });
 
     app.get('/username_edit', async (req, res) => {
         if (!isValidSession(req)) {
             res.redirect('/login');
             return;
         }
+        //get user profile from database
         user_profile = await userCollection.findOne({ username: req.session.user.username });
+        //get user id
         const userID = user_profile.username;
-        res.render('username_edit', { userID: userID })
-    })
+        res.render('username_edit', { userID: userID });
+    });
 
     app.post('/username_edit', async (req, res) => {
+        //validate input
         const schema = Joi.object({
             new_UserID: Joi.string().alphanum().min(3).max(20).required(),
         });
+        //get user id
         var { new_UserID } = req.body;
         const validationResult = schema.validate({
             new_UserID,
@@ -82,43 +88,47 @@ module.exports = async function (app, Joi, bcrypt, saltRounds) {
         //update user profile
         if (!validationResult.error) {
             if (new_UserID === req.session.user.username) {
-                res.render('templates/notification_page.ejs', {message:'Username is the same as the current one.'})
+                res.render('templates/notification_page.ejs', {message:'Username is the same as the current one.'});
                 return;
             }
             else if (await userCollection.findOne({ username: new_UserID })) {
-                res.render('templates/notification_page.ejs', {message:'Username already exists.'})
+                res.render('templates/notification_page.ejs', {message:'Username already exists.'});
                 return;
             }
             else if (new_UserID === 'admin'|| new_UserID === 'Admin') {
-                res.render('templates/notification_page.ejs', {message:'Username cannot be admin.'})
+                res.render('templates/notification_page.ejs', {message:'Username cannot be admin.'});
                 return;
             }
             else{
+                //update database
                 await userCollection.updateOne(
                     { username: req.session.user.username },
                     { $set: { username: new_UserID } }
                 );
                 req.session.user.username = new_UserID;
                 //update session
-                res.render('templates/notification_page.ejs', {message:'Username has been updated.'})
+                res.render('templates/notification_page.ejs', {message:'Username has been updated.'});
             }
         }
         else{
-            res.render('templates/notification_page.ejs', {message:'Username must be between 3 and 20 characters long.'})
+            res.render('templates/notification_page.ejs', {message:'Username must be between 3 and 20 characters long.'});
         }
-    })
+    });
 
     app.get('/password_edit', async (req, res) => {
         if (!isValidSession(req)) {
             res.redirect('/login');
             return;
         }
+        //get user profile from database
         user_profile = await userCollection.findOne({ username: req.session.user.username });
+        //get user password
         const password = user_profile.password;
-        res.render('password_edit', { password: password })
-    })
+        res.render('password_edit', { password: password });
+    });
 
     app.post('/password_edit', async (req, res) => {
+        //validate input
         const schema = Joi.object({
             new_password: Joi.string()
             .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{3,30}$/)
@@ -140,13 +150,13 @@ module.exports = async function (app, Joi, bcrypt, saltRounds) {
         if (!validationResult.error) {
             // check if current password is the same as session password
             if (!await bcrypt.compare(current_password, req.session.user.password)) {
-                res.render('templates/notification_page.ejs', {message:'Current password is incorrect.'})
+                res.render('templates/notification_page.ejs', {message:'Current password is incorrect.'});
                 return;
             }
             else{
                 // check if new password is the same as session password
                 if (await bcrypt.compare(new_password, req.session.user.password)) {
-                    res.render('templates/notification_page.ejs', {message:'New password is the same as the current one.'})
+                    res.render('templates/notification_page.ejs', {message:'New password is the same as the current one.'});
                     return;
                 }
                 else{
@@ -156,31 +166,31 @@ module.exports = async function (app, Joi, bcrypt, saltRounds) {
                         await userCollection.updateOne(
                             { username: req.session.user.username },
                             { $set: { password: new_password } }
-                        )
+                        );
                         req.session.user.password = new_password;
                         //update session
-                        res.render('templates/notification_page.ejs', {message:'Password has been updated.'})
+                        res.render('templates/notification_page.ejs', {message:'Password has been updated.'});
                     } else {
-                        res.render('templates/notification_page.ejs', {message:'Passwords do not match. Please try again.'})
+                        res.render('templates/notification_page.ejs', {message:'Passwords do not match. Please try again.'});
                     }
                 }
             }
         }
         else{
-            res.render('templates/notification_page.ejs', {message:'Password format invalid, please try again.'})
+            res.render('templates/notification_page.ejs', {message:'Password format invalid, please try again.'});
         }
-    })
+    });
 
     app.post('/delete', async (req, res) => {
-        console.log("delete")
-        var build = JSON.parse(req.body.build)
+        console.log("delete");
+        var build = JSON.parse(req.body.build);
         var existingUser = await userCollection.findOne({ username: req.session.user.username });
-        console.log(build in existingUser.favourites)
+        console.log(build in existingUser.favourites);
         //delete the build from the user's favourites
         await userCollection.updateOne(
             { username: req.session.user.username },
             { $pull: { favourites: build } }
-        )
+        );
         res.redirect('/profile');
-    })
-}
+    });
+};
